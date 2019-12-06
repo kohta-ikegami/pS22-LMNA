0d2ffee0ed9cab2c76e40ca5e0f65b08e242fdc7
Kohta Ikegami <58574051+kohta-ikegami@users.noreply.github.com>
2019-12-05 18:56:20 -0600

    Update chromcheck.sh

--- chromcheck.sh
+++ chromcheck.sh
@@ -20,9 +20,3 @@ res=$2
 sed -n '0~'$res'p' $infile | cut -f1 | uniq
 
 exit
-
-###########################################
-# Copyright (c) 2017, Kohta Ikegami
-# All rights reserved.
-# contact: ikgmk@uchicago.edu
-###########################################	
