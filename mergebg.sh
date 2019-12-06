871f723096d60308c7d6a52404d55aeee1a1dc6d
Kohta Ikegami <58574051+kohta-ikegami@users.noreply.github.com>
2019-12-05 18:54:08 -0600

    Add files via upload

---
+++ mergebg.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+## mergebg.sh
+
+
+if [ $# -lt 1 ]  
+then
+	echo -e "\n"
+	echo -e "Tool:   \t`basename $0`"
+	echo -e "Author: \tKohta Ikegami, 2017"
+	echo -e "Version:\tv1.0"
+	echo -e "Summary:\tMerge neighboring bed regions with identical scores.\n"
+	echo -e "Usage:  \t`basename $0` [bed] > [stdout]\n"
+
+	exit 
+fi
+
+# 1) Files and variables
+	bed=$1
+	
+# 2) Processing
+
+	awk 'BEGIN {chr=0} 
+		{	if(chr == 0) {chr=$1; start=$2; stop=$3; score=$4; next;}
+			else if ($1== chr && $2 == stop && $4 == score) {stop=$3; next;}
+			else {OFS="\t"; print chr, start, stop, score; chr=$1; start=$2; stop=$3; score=$4; next;}
+		}
+		END {OFS="\t"; print chr, start, stop, score;}' $bed
+exit;
+		
+###########################################
+# Copyright (c) 2017, Kohta Ikegami
+# All rights reserved.
+# contact: ikgmk@uchicago.edu
+###########################################	
